4ff2e1af40f942f4391b8d1f00e4b669c453208b
Leowtha <104984797+Leowtha@users.noreply.github.com>
2026-04-25 02:04:59 -0500

    Add files via upload

---
+++ modules/swffg-migration.js
@@ -0,0 +1,120 @@
+/**
+ * Handles all logic related to migrating the system to a new version, including sending notifications
+ * @returns {Promise<void>}
+ */
+export async function handleUpdate() {
+  const registeredVersion = game.settings.get("ucttg", "systemMigrationVersion");
+  const runningVersion = game.system.version;
+  if (registeredVersion !== runningVersion) {
+    await handleMigration(registeredVersion, runningVersion);
+    await sendChanges(runningVersion);
+    await game.settings.set("ucttg", "systemMigrationVersion", runningVersion);
+  }
+}
+
+/**
+ * Handles migration logic for the system
+ * @param oldVersion - version previously run (from the settings)
+ * @param newVersion - version currently running (from game.system.version)
+ * @returns {Promise<void>}
+ */
+async function handleMigration(oldVersion, newVersion) {
+  // migration handlers should be added here going forward
+  await migrateTo1_901();
+  if (parseFloat(oldVersion) < 1.906) {
+    await migrateTo1_906();
+  }
+  await warnTheme();
+}
+
+/**
+ * Sends a notification to all users in the game that the system has been updated
+ * @param newVersion - version currently running (from game.system.version)
+ * @returns {Promise<void>}
+ */
+async function sendChanges(newVersion) {
+  const template = "systems/ucttg/templates/notifications/new_version.html";
+  const html = await renderTemplate(template, { version: newVersion });
+  const messageData = {
+    user: game.user.id,
+    type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+    content: html,
+  };
+  ChatMessage.create(messageData);
+}
+
+/**
+ * Notify users if they are using the now-retired theme
+ * @returns {Promise<void>}
+ */
+async function warnTheme() {
+  if (game.settings.get("ucttg", "ui-uitheme") === "default") {
+    const messageData = {
+      user: game.user.id,
+      type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+      content: "You are using an unsupported theme. Expected issues, or swap to the Mandar theme.<br>(This message will only show once.)",
+    };
+    ChatMessage.create(messageData);
+  }
+}
+
+/**
+ * Handles updating talents from species on actors to be a "species" talent rather than the default type
+ * @returns {Promise<void>}
+ */
+async function migrateTo1_901() {
+  for (const actor of game.actors) {
+    for (const species of actor.items.filter(a => a.type === "species")) {
+      for (const talent of Object.values(species.system.talents)) {
+        await actor.items.find(i => i.name === talent.name)?.update({flags: {ucttg: {fromSpecies: true}}});
+      }
+    }
+  }
+}
+
+/**
+ * Updates settings pointing to system compendiums to instead point to world-level compendiums
+ * @returns {Promise<void>}
+ */
+async function migrateTo1_906() {
+  // specializations
+  let compendiums = [];
+  for (const compendium of game.settings.get("ucttg", "specializationCompendiums").split(",")) {
+    if (compendium.includes("ucttg.")) {
+      compendiums.push(compendium.replace("ucttg.", "world."));
+    } else {
+      compendiums.push(compendium);
+    }
+  }
+  game.settings.set("ucttg", "specializationCompendiums", compendiums.join(","));
+  // signature abilities
+  compendiums = [];
+  for (const compendium of game.settings.get("ucttg", "signatureAbilityCompendiums").split(",")) {
+    if (compendium.includes("ucttg.")) {
+      compendiums.push(compendium.replace("ucttg.", "world."));
+    } else {
+      compendiums.push(compendium);
+    }
+  }
+  game.settings.set("ucttg", "signatureAbilityCompendiums", compendiums.join(","));
+  // force powers
+  compendiums = [];
+  for (const compendium of game.settings.get("ucttg", "forcePowerCompendiums").split(",")) {
+    if (compendium.includes("ucttg.")) {
+      compendiums.push(compendium.replace("ucttg.", "world."));
+    } else {
+      compendiums.push(compendium);
+    }
+  }
+  game.settings.set("ucttg", "forcePowerCompendiums", compendiums.join(","));
+  // talents
+  compendiums = [];
+  for (const compendium of game.settings.get("ucttg", "talentCompendiums").split(",")) {
+    if (compendium.includes("ucttg.")) {
+      compendiums.push(compendium.replace("ucttg.", "world."));
+    } else {
+      compendiums.push(compendium);
+    }
+  }
+  game.settings.set("ucttg", "talentCompendiums", compendiums.join(","));
+}
