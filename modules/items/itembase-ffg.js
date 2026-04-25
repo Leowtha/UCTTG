8ad74759af9a69d4ef1da84b2c3009f34f87c2ec
Leowtha <104984797+Leowtha@users.noreply.github.com>
2026-04-25 01:54:35 -0500

    Reset

--- modules/items/itembase-ffg.js
+++
@@ -1,8 +0,0 @@
-import ItemHelpers from "../helpers/item-helpers.js";
-
-export default class ItemBaseFFG extends Item {
-  async update(data, options = {}) {
-    if (!Object.keys(data).includes("ownership") && typeof data.flags?.clickfromparent === "undefined" && typeof this.flags?.clickfromparent !== "undefined") data.flags.clickfromparent = this.flags.clickfromparent
-    await super.update(ItemHelpers.normalizeDataStructure(data), options);
-  }
-}
